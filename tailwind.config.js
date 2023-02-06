b19b1ee010410a21404a59f9477b34e4052802f6
hxutixnnn <ng.huutien98@gmail.com>
2023-02-06 16:56:51 +0700

    fix: tailwind doesn't apply on `/components`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   plugins: [
     require("@tailwindcss/line-clamp"),
     require("tailwind-scrollbar")({ nocompatible: true }),
