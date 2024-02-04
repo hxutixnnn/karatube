47fce78a21b26a0667c3b4ad8518434392ba106e
Tien Huu Nguyen <tienguyen@strongtie.com>
2024-02-04 12:15:54 +0700

    fix(safari): input zoom on focus

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: { children: any }) {
         <link rel="shortcut icon" href="/favicon.svg" />
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+          content="minimum-scale=1, initial-scale=1, maximum-scale=1, width=device-width, user-scalable=no"
         />
       </head>
       <body
